5fae35275bfa94a77923f3a3703bb4abe570f43c
Mallcom <mallcumek@gmail.com>
2024-01-17 13:41:22 +0100

    Zprovozněny komentare k prispevku podle tutorialu. Zapis, vypi - test OK.

--- app/Module/Admin/Presenters/PostPresenter.php
+++ app/Module/Admin/Presenters/PostPresenter.php
@@ -27,7 +27,48 @@ final class PostPresenter extends Nette\Application\UI\Presenter
         if (!$post) {
             $this->error('Stránka nebyla nalezena');
         }
-
+        // Uložení příspěvku do šablony
         $this->template->post = $post;
+        // Uložení komentáře do šablony
+        $this->template->comments = $post->related('comments')->order('created_at');
     }
+        // Továrna na formulář v Presenteru
+    protected function createComponentCommentForm(): Form
+    {
+        $form = new Form; // means Nette\Application\UI\Form
+
+        $form->addText('name', 'Jméno:')
+            ->setRequired();
+
+        $form->addEmail('email', 'E-mail:');
+
+        $form->addTextArea('content', 'Komentář:')
+            ->setRequired();
+
+        $form->addSubmit('send', 'Publikovat komentář');
+        $form->onSuccess[] = $this->commentFormSucceeded(...);
+
+        return $form;
+    }
+
+    //Tato nová metoda má jeden argument, což je instance formuláře, který byl odeslán – vytvořen továrnou.
+    // Odeslané hodnoty získáme ve $data. A následně uložíme data do databázové tabulky comments.
+    private function commentFormSucceeded(\stdClass $data): void
+    {
+        $postId = $this->getParameter('postId');
+
+        $this->database->table('comments')->insert([
+            'post_id' => $postId,
+            'name' => $data->name,
+            'email' => $data->email,
+            'content' => $data->content,
+        ]);
+
+        $this->flashMessage('Děkuji za komentář', 'success');
+        $this->redirect('this');
+    }
+
+
+
+
 }
