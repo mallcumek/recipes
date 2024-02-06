3112ad55420f3c3ec77a98cc140e3e3cbd96c523
Mallcom <mallcumek@gmail.com>
2024-02-06 17:08:54 +0100

    Nastaven router na seo title kategorii. Zmenily se automaticky spravne odkazy na kategorie v @layout.latte. Chybí stránkování.

--- app/Router/RouterFactory.php
+++ app/Router/RouterFactory.php
@@ -20,10 +20,13 @@ final class RouterFactory
 		$router = new RouteList;
         $router->addRoute('/login', 'Admin:Sign:in');
         $router->addRoute('/registration', 'Admin:Sign:up');
+        // funguje na hezke url u postu
         $router->addRoute('/recipe/<postId>-<seotitle>', 'Admin:Post:show');
+        // Tato routa na kategore zmenila automaticky odkazy v @layout.latte n:href="Category:show $cat->category_seotitle"
+        $router->addRoute('/<category_seotitle>/', 'Admin:Category:show');
 		// Default route that maps to the Admin Dashboard
         $router->addRoute('<presenter>/<action>', 'Admin:Homepage:default');
-		//default route : $router->addRoute('<presenter>/<action>', 'Admin:Dashboard:default');
+		// Default route : $router->addRoute('<presenter>/<action>', 'Admin:Dashboard:default');
 
 
 		return $router;
